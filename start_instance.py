8e89659064a35b9bb1546696a7132b057c7d8596
Sulabh Shrestha <tsulabh4@gmail.com>
2021-05-29 14:17:18 +0545

    - Add Instance ID through yaml

--- start_instance.py
+++ start_instance.py
@@ -2,13 +2,18 @@ import time
 import json
 import boto3
 import logging
+import configparser
 logging.basicConfig(level=logging.INFO)
 ec2 = boto3.client('ec2', region_name='us-east-1')
+config = configparser.ConfigParser()
+config.read('credentials.ini')
+
+instance_id = config['CREDENTIALS']['instance_id']
 
 def handler(event, context):
-    ec2.start_instances(InstanceIds=[''])
+    ec2.start_instances(InstanceIds=[instance_id])
     time.sleep(5)
-    reservations = ec2.describe_instances(InstanceIds=['']).get("Reservations")
+    reservations = ec2.describe_instances(InstanceIds=[instance_id]).get("Reservations")
     for reservation in reservations:
         for instance in reservation['Instances']:
             print(instance.get("PublicIpAddress"))
