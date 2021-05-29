06642d9a5e6f70c1fb0c08eab31b5069885b196d
Sulabh Shrestha <tsulabh4@gmail.com>
2021-05-29 14:31:42 +0545

    - Add Environmental Variables

--- start_instance.py
+++ start_instance.py
@@ -1,3 +1,4 @@
+import os
 import time
 import json
 import boto3
@@ -5,10 +6,11 @@ import logging
 import configparser
 logging.basicConfig(level=logging.INFO)
 ec2 = boto3.client('ec2', region_name='us-east-1')
-config = configparser.ConfigParser()
-config.read('credentials.ini')
+# config = configparser.ConfigParser()
+# config.read('credentials.ini')
 
-instance_id = config['CREDENTIALS']['instance-id']
+# instance_id = config['CREDENTIALS']['instance-id']
+instance_id = os.environ['InstanceId']
 
 def handler(event, context):
     ec2.start_instances(InstanceIds=[instance_id])
